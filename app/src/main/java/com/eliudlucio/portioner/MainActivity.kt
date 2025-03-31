36b3137446f59217174fb19d031c298a63112403
Eliud Lucio <EliudJonathe@outlook.com>
2025-03-31 16:29:50 -0600

    Modificaciones menores

--- app/src/main/java/com/eliudlucio/portioner/MainActivity.kt
+++ app/src/main/java/com/eliudlucio/portioner/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Forzar modo claro
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         // Establecer la Toolbar como el App Bar
         val toolbar: Toolbar = findViewById(R.id.my_toolbar)
@@ -29,10 +33,12 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_config -> {
+                //TODO
                 Toast.makeText(this, "Abrir Configuración", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.menu_help -> {
+                //TODO
                 Toast.makeText(this, "Abrir Ayuda", Toast.LENGTH_SHORT).show()
                 true
             }
