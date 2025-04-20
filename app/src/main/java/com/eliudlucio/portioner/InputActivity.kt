a6e251c6b936fed10633bbc19c152a5ff592661d
Eliud Lucio <EliudJonathe@outlook.com>
2025-04-20 16:02:59 -0600

    Implementando SeekBar para Porcentaje

--- app/src/main/java/com/eliudlucio/portioner/InputActivity.kt
+++ app/src/main/java/com/eliudlucio/portioner/InputActivity.kt
@@ -1,14 +1,9 @@
 package com.eliudlucio.portioner
 
 import android.os.Bundle
-import android.view.View
 import android.widget.SeekBar
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.eliudlucio.portioner.databinding.ActivityMainBinding
 
 class InputActivity : AppCompatActivity() {
 
@@ -34,14 +29,35 @@ class InputActivity : AppCompatActivity() {
 //            }
 //        }
 
-        val valueTextView = findViewById<TextView>(R.id.tv_portions_number)
+        val tvValuePortion = findViewById<TextView>(R.id.tv_portions_number)
         val portionSeekBar = findViewById<SeekBar>(R.id.portionSeekBar)
 
-        valueTextView.text = "1"
+        tvValuePortion.text = "1"
 
         portionSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                valueTextView.text = "${progress + 1}"
+                tvValuePortion.text = "${progress + 1}"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Opcional: Aquí puedes hacer algo cuando se empieza a mover el SeekBar
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Opcional: Aquí puedes hacer algo cuando se deja de mover el SeekBar
+            }
+        })
+
+        val tvValuePercentage = findViewById<TextView>(R.id.tv_percentage_number)
+        val percentageSeekBar = findViewById<SeekBar>(R.id.percentageSeekBar)
+
+        tvValuePercentage.text = "50%"
+        percentageSeekBar.progress = percentageSeekBar.max / 2
+
+        percentageSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val percent = progress * 5
+                tvValuePercentage.text = "$percent%"
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
